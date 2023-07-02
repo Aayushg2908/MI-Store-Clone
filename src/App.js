76786afcfef4b10ecb12015812ddba388124cfdf
Aayush <aayushg2908@gmail.com>
2023-07-02 20:38:24 +0530

    MI Store clone using React

--- src/App.js
+++ src/App.js
@@ -1,24 +1,92 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import "./App.css";
+import PreNavbar from "./components/PreNavbar";
+import Navbar from "./components/Navbar";
+import Slider from "./components/Slider";
+import pkg from "./data/data.json";
+import Offers from "./components/Offers";
+import Header from "./components/Heading.js";
+import StarProduct from "./components/StarProduct.js";
+import HotAccessoriesMenu from "./components/HotAccessoriesMenu.js";
+import HotAccessories from "./components/HotAccessories.js";
+import ProductReviews from "./components/ProductReviews.js";
+import Videos from "./components/Videos.js"
+import Banner from "./components/Banner.js"
+import Footer from "./components/Footer.js"
+import NavOptions from "./components/NavOptions.js"
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <PreNavbar />
+      <Navbar />
+      <NavOptions miPhones={pkg.miPhones} redmiPhones={pkg.redmiPhones} tv={pkg.tv} laptop={pkg.laptop} fitnessAndLifeStyle={pkg.fitnessAndLifeStyle} home={pkg.home} audio={pkg.audio}  accessories={pkg.accessories}/>
+      <Slider start={pkg.banner.start} />
+      <Offers offers={pkg.offer} />
+      <Header text="STAR PRODUCTS" />
+      <StarProduct StarProduct={pkg.starProduct} />
+      <Header text="HOT ACCESSORIES" />
+      <HotAccessoriesMenu />
+      <Routes>
+        <Route
+          exact
+          path="/music"
+          element={
+            <HotAccessories
+              music={pkg.hotAccessories.music}
+              musicCover={pkg.hotAccessoriesCover.music}
+            />
+          }
+        />
+        <Route
+          exact
+          path="/smartDevice"
+          element={
+            <HotAccessories
+              smartDevice={pkg.hotAccessories.smartDevice}
+              smartDeviceCover={pkg.hotAccessoriesCover.smartDevice}
+            />
+          }
+        />
+        <Route
+          exact
+          path="/home"
+          element={
+            <HotAccessories
+              home={pkg.hotAccessories.home}
+              homeCover={pkg.hotAccessoriesCover.home}
+            />
+          }
+        />
+        <Route
+          exact
+          path="/lifestyle"
+          element={
+            <HotAccessories
+              lifeStyle={pkg.hotAccessories.lifeStyle}
+              lifeStyleCover={pkg.hotAccessoriesCover.lifeStyle}
+            />
+          }
+        />
+        <Route
+          exact
+          path="/mobileAccessories"
+          element={
+            <HotAccessories
+              mobileAccessories={pkg.hotAccessories.mobileAccessories}
+              mobileAccessoriesCover={pkg.hotAccessoriesCover.mobileAccessories}
+            />
+          }
+        />
+      </Routes>
+      <Header text="PRODUCT REVIEWS" />
+      <ProductReviews productReviews={pkg.productReviews} />
+      <Header text="VIDEOS" />
+      <Videos videos={pkg.videos} />
+      <Header  text="IN THE PRESS"/>
+      <Banner banner={pkg.banner} />
+      <Footer footer={pkg.footer} />
+    </Router>
   );
 }
 
---
+++ src/components/Videos.js
@@ -0,0 +1,22 @@
+import React from "react";
+import VideoCard from "./VideoCard.js"
+import "../styles/Videos.css";
+
+const Videos = ({ videos }) => {
+  return (
+    <div>
+      <div className="videos">
+        {videos.map((item, index) => (
+          <VideoCard
+            index={index}
+            key={item.image}
+            image={item.image}
+            name={item.name}
+          />
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default Videos;
